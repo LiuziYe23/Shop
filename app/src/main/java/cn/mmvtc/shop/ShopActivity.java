903c991fbe7739c9780923f3bff915b5a81e08fa
feng <846783735@qq.com>
2020-01-09 10:22:12 +0800

    Initial commit

---
+++ app/src/main/java/cn/mmvtc/shop/ShopActivity.java
@@ -0,0 +1,108 @@
+package cn.mmvtc.shop;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class ShopActivity extends Activity implements View.OnClickListener{
+    private ListView mListView;
+    private ImageView add,car;
+    //商品名称与价格数据集合
+    private String[] titles = {"百香果", "草莓", "橙子", "火龙果", "橘子",
+            "柠檬", "荔枝", "牛油果", "苹果", "石榴"};
+
+    private String[] prices = {"20元/Kg", "21元/Kg", "22元/Kg", "33元/Kg", "44元/Kg",
+            "25元/Kg", "26元/Kg", "17元/Kg", "28元/Kg", "39元/Kg"};
+
+    private int[] icons = {R.drawable.baixiangguo,R.drawable.caomei,R.drawable.chengzi,R.drawable.huolongguo,R.drawable.juzi,
+            R.drawable.lemon,R.drawable.lizhi,R.drawable.niuyouguo,R.drawable.pingguo,R.drawable.shiliu};
+
+    SQLiteHelper sqLiteHelper;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop);
+        mListView = (ListView) findViewById(R.id.lv);//初始化ListView控件
+        add = (ImageView) findViewById(R.id.add);
+        car = (ImageView) findViewById(R.id.car);
+        MyBaseAdapter mAdapter = new MyBaseAdapter();
+        mListView.setAdapter(mAdapter);
+        initData();
+    }
+    protected void initData(){sqLiteHelper = new SQLiteHelper(this);}
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.car:
+                Intent intent = new Intent(ShopActivity.this,CarActivity.class);
+                startActivityForResult(intent,1);
+        }
+    }
+     class MyBaseAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return titles.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return titles[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder vh;
+            if (convertView == null){
+                convertView = View.inflate(ShopActivity.this,R.layout.shop_item,null);
+                vh = new ViewHolder();
+                vh.tvShopTitle = (TextView) convertView.findViewById(R.id.title);
+                vh.tvShopPrice = (TextView) convertView.findViewById(R.id.price);
+                vh.tvShopiv = (ImageView) convertView.findViewById(R.id.iv);
+                vh.tvShopadd = (ImageView) convertView.findViewById(R.id.add);
+                convertView.setTag(vh);
+                final ViewHolder finalVh = vh;
+                vh.tvShopadd.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v){
+                        String CarTitle = finalVh.tvShopTitle.getText().toString();
+                        String CarPrice = finalVh.tvShopPrice.getText().toString();
+                        if (sqLiteHelper.insertData(CarTitle,CarPrice)){
+                            Toast.makeText(ShopActivity.this,"添加成功",Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(ShopActivity.this,"添加失败",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }else{
+                vh = (ViewHolder)convertView.getTag();
+            }
+            vh.tvShopTitle.setText(titles[position]);
+            vh.tvShopPrice.setText(prices[position]);
+            vh.tvShopiv.setBackgroundResource(icons[position]);
+            return convertView;
+        }
+        class ViewHolder {
+            TextView tvShopTitle;
+            TextView tvShopPrice;
+            ImageView tvShopadd;
+            ImageView tvShopiv;
+        }
+    }
+}
+
